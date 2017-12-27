f1d3ce2efa39bfd0186569eb0f283f3c2101f61e
MrBoutte101 <mrboutte21@gmail.com>
2017-12-27 18:18:41 -0500

    Added dependencies express and express-graphql
    server and schema WIP

--- index.js
+++ index.js
@@ -1,54 +1,113 @@
-const { graphql, buildSchema } = require('graphql');
-// import { graphql, buildSchema } from 'graphql';
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLInt,
+  GraphQLString,
+  GraphQLBoolean,
+} = require('graphql');
 
-const schema = buildSchema(`
-type Question {
-  id: ID,
-  question_english: String,
-  question_spanish: String,
-  question_chinese: String,
-  favorite: Boolean,
-}
+const PORT = process.env.PORT || 3000;
+const server = express();
 
-type Answer {
-  id: ID,
-  question_id: Int,
-  answer_english: String,
-  answer_spanish: String,
-  answer_chinese: String,
-}
+const questionType = new GraphQLObjectType({
+  name: 'Question',
+  description: 'A question from the naturalization civics aid exam.',
+  fields: {
+    id: {
+      type: GraphQLID,
+      description: 'The id of the question.',
+    },
+    question_english: {
+      type: GraphQLString,
+      description: 'The english translation of the question.',
+    },
+    question_spanish: {
+      type: GraphQLString,
+      description: 'The spanish translation of the question.',
+    },
+    question_chinese: {
+      type: GraphQLString,
+      description: 'The chinese translation of the question.',
+    },
+    favorite: {
+      type: GraphQLBoolean,
+      description: 'Whether or not the question has been saved as a favorite',
+    },
+  },
+});
 
-type Query {
-  question: Question
-}
+const queryType = new GraphQLObjectType({
+  name: 'QueryType',
+  description: 'The root query type.',
+  fields: {
+    question: {
+      type: questionType,
+      resolve: () =>
+        new Promise(resolve => {
+          resolve({
+            id: '6',
+            question_english: 'blah blah in english',
+            question_spanish: 'blah blah in spanish',
+            question_chinese: 'blah blah in chinese',
+            favorite: true,
+          });
+        }),
+    },
+  },
+});
 
-type Schema {
-  query: Query
-}
-`);
+const schema = new GraphQLSchema({
+  query: queryType,
+  // mutation,
+  // subscription,
+});
 
-const resolvers = {
-  question: () => ({
-    id: '6',
-    question_english: 'question in english',
-    question_spanish: 'question in spanish',
-    question_chinese: 'question in chinese',
-    favorite: true,
-  }),
+const question1 = {
+  id: '6',
+  question_english: 'blah blah in english',
+  question_spanish: 'blah blah in spanish',
+  question_chinese: 'blah blah in chinese',
+  favorite: true,
 };
 
-const query = `
-query myFirstQuery {
-  question {
-    id,
-    question_english,
-    question_spanish,
-    question_chinese,
-    favorite,
-  }
-}
-`;
+const question2 = {
+  id: '7',
+  question_english: 'zzzblah blah in english',
+  question_spanish: 'zzzblah blah in spanish',
+  question_chinese: 'zzzblah blah in chinese',
+  favorite: false,
+};
 
-graphql(schema, query, resolvers)
-  .then(result => console.log(result))
-  .catch(error => console.log(error));
+const answer1 = {
+  id: '3',
+  question_id: '6',
+  answer_english: 'answer blah blah in english',
+  answer_spanish: 'answer blah blah in spanish',
+  answer_chinese: 'answer blah blah in chinese',
+};
+
+const answer2 = {
+  id: '4',
+  question_id: '6',
+  answer_english: 'answer zzzblah blah in english',
+  answer_spanish: 'answer zzzblah blah in spanish',
+  answer_chinese: 'answer zzzblah blah in chinese',
+};
+
+const questions = [question1, question2];
+const answers = [answer1, answer2];
+
+server.use(
+  '/graphql',
+  graphqlHTTP({
+    schema,
+    graphiql: true,
+  }),
+);
+
+server.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
